531f9e04cb2e2503bb1fd23f62c671c89886e6fe
SharkySK <jaku0584@stud.kea.dk>
2019-04-08 16:45:24 +0000

    ok

--- restapi/urls.py
+++ restapi/urls.py
@@ -4,10 +4,11 @@ from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'facility', views.FacilityViewSet)
-
+router.register(r'bridge', views.BridgeViewSet)
 # Wire up our API using automatic URL routing
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
+    path('admin/', admin.site.urls)
     path('', include(router.urls))
 ]
