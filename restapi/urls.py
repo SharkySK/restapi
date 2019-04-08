d83c2f8a0fce75f3b75d32c79d179988cfd95af3
SharkySK <jaku0584@stud.kea.dk>
2019-04-08 18:52:52 +0200

    Urls fix

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from quickstart import views
@@ -5,10 +6,20 @@ from quickstart import views
 router = routers.DefaultRouter()
 router.register(r'facility', views.FacilityViewSet)
 router.register(r'bridge', views.BridgeViewSet)
+router.register(r'equipment', views.EquipmentViewSet)
+router.register(r'user_account', views.User_accountViewSet)
+router.register(r'activity_log', views.Activity_logViewSet)
+router.register(r'booking_schedule', views.Booking_scheduleViewSet)
+router.register(r'booking_opt', views.Booking_optViewSet)
+router.register(r'training', views.TrainingViewSet)
+router.register(r'package', views.Package_optViewSet)
+router.register(r'billing_opt', views.Billing_optViewSet)
+router.register(r'opening_hours', views.Opening_hoursViewSet)
+
 # Wire up our API using automatic URL routing
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
